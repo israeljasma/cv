7570a17224dbf23bb34baf3df8a90873471778d2
israeljasmac <35226248+israeljasmac@users.noreply.github.com>
2018-01-15 10:19:49 -0300

    Update index.js
    cambios

--- index.js
+++ index.js
@@ -1,34 +1,106 @@
+const port = process.env.PORT || 80;
 const express = require('express');
-const app = express();
-const port = process.env.PORT || 80
-
 const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-
 // realizando conexion con la bd
-mongoose.connect('mongodb://localhost/pruebas', { useMongoClient: true });
+mongoose.connect('mongodb://localhost/productos', { useMongoClient: true });
 
-var contenidoSchemaJSON = {
-    titulo: String,
-    contenido: String
+var productosSchemaJSON = {
+    nombre: String,
+    fabricante: String
 };
 
 // Creando Schema
-var contenidoSchema = new Schema(contenidoSchemaJSON);
+var productosSchema = new Schema(productosSchemaJSON);
 
 // Creando modelo
-var Contenido = mongoose.model("prueba01",contenidoSchema);
+var Productos = mongoose.model("productos",productosSchema);
 
+//Lista los productos
 app.get("/list",(req,res) => {
-	Contenido.find((err, result) => {
+	Productos.find((err, result) => {
 		console.log(result);
 		res.send(result);
 	});
 });
 
+//Muestra los productos
+app.get('/productos', (req, res) => {
+	Productos.find({}, (err, productos) => {
+        if(err) return res.status(500).send({message: 'Error al realizar peticion: ${err}'});
+        //if(!test) return res.status(404).send({message: 'No existe'});
+
+        res.status(200).send({productos});
+        console.log(productos);
+    });
+});
+
+// Muestra un producto especifico
+app.get('/productos/:_id', (req, res) => {
+    let productosId = req.params._id;
+
+    Productos.findById(productosId, (err, producto) => {
+    	if(err) return res.status(500).send({message: `Error al realizar peticion: ${err}`});
+    	if(!productos) return res.status(400).send({message: 'El usuario no existe'});
+    	res.status(200).send({productos : producto});
+	console.log(producto);
+    });
+});
+
+// Registrar un nuevo producto
+app.post('/agregar', (req, res) => {
+	console.log('POST /agregar');
+	console.log(req.body);
+
+	let productos = new Productos();
+	productos.nombre = req.body.nombre;
+	productos.fabricante = req.body.fabricante;
+
+	productos.save((err, productosAGuardar) => {
+		if(err) res.status(500).send({message: `Error al guardar en la base de datos: ${err} `});
+		
+		res.status(200).send({productos: productosAGuardar});
+		//console.log(usuarioAGuardar);
+	});
+});
+
+// Modificar un producto
+app.put('/actualizar/:_id', (req, res) => {
+    let productosId = req.params._id;
+    let update = req.body;
+
+    Productos.findByIdAndUpdate(productosId, update, (err, productosUpdate) => {
+    	if(err) return res.status(500).send({message: `Error al actualizar el producto: ${err}`});
+
+   	res.status(200).send({ Productos: productosUpdate });
+	console.log(productosUpdate);
+    });
+});
 
 
+//fd
+// Eliminar un producto
+app.delete('/eliminar/:_id', (req, res) => {
+	let productosId = req.params._id; 
+	Productos.findById(productosId, (err, productos) => {
+		if(err) res.status(500).send({message: 'Error al borrar la cosa: ${err}'});
+		
+		productos.remove(err => {
+			if(err) res.status(500).send({message: 'Error al borrar la cosa: ${err}'});
+			res.status(200).send({message: 'La cosa fue eliminada con exito'});
+		});
+	});
+});
+
+
+
+
+
+
+ 
 var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: 'localhost',
